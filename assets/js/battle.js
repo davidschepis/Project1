d87e8a891336ef0881eb1e31635626eeaa2876ac
David Schepis <davidschepis@gmail.com>
2021-12-21 15:48:49 -0700

    cleaned up code, added battle music

--- assets/js/battle.js
+++ assets/js/battle.js
@@ -1,15 +1,12 @@
 var battleSection = $('#battleSection');
 var key = "98c7599992msh5ea6f0b3a380ff5p13b5bbjsn24e983bd9684";
 
-
-
 showBattleScreen()
-
 
 function showBattleScreen() {
     var randoPokemon
     var url = "https://pokedex2.p.rapidapi.com/pokedex/us/";
-    
+
     fetch(url, {
         "method": "GET",
         "headers": {
@@ -20,16 +17,13 @@ function showBattleScreen() {
         return response.json();
     }).then(function (data) {
         var number = Math.floor(Math.random() * data.length)
-        var outputstring = "<img src='"+data[number].ThumbnailImage+"'>";
+        var outputstring = "<img src='" + data[number].ThumbnailImage + "'>";
         console.log(number)
         console.log(outputstring)
         document.getElementById("battleSection2").innerHTML = outputstring;
 
         var number = Math.floor(Math.random() * data.length)
-        outputstring = "<img src='"+data[number].ThumbnailImage+"'>";
+        outputstring = "<img src='" + data[number].ThumbnailImage + "'>";
         document.getElementById("battleSection1").innerHTML = outputstring;
-
     });
-    
-    
-    }
+}
