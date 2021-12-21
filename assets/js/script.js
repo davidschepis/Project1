2849d9b358e8200afb6be8c6fb0a46a2eb3ba5d0
David Schepis <davidschepis@gmail.com>
2021-12-21 11:42:40 -0700

    search page now shows name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var mainSection = $('#mainSection');
+var key = "67cb78248emsh1e4e33cf18493bcp19763bjsne115c744c684";
 
 
 document.getElementById("searchButton").addEventListener("click", handleSearch);
@@ -8,7 +9,6 @@ function handleSearch() {
 	var pokename = document.getElementById("searchText").value;
 	
 	var url = "https://pokedex2.p.rapidapi.com/pokedex/us/" + pokename;
-	var key = "67cb78248emsh1e4e33cf18493bcp19763bjsne115c744c684";
 
 	fetch(url, {
 		"method": "GET",
@@ -20,8 +20,12 @@ function handleSearch() {
 		return response.json();
 	}).then(function (data) {
 		console.log(data);
-		
-		document.getElementById("mainSection").innerHTML = "<h1>" + data[0].name + "</h1>";
+		var outputstring = "";
+		outputstring += "<h1>The pokemon's name is: " + data[0].name + "</h1>";
+		for (var i = 0; i < data[0].type.length; i++) {
+			outputstring += "<h1>The pokemon's type is: " + data[0].type[i] + "</h1>";
+		}
+		document.getElementById("result").innerHTML = outputstring;
 	});
 }
 
