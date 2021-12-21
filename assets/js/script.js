239176f5a34f54403747aacef588d1e6c9b4c6f9
David Schepis <davidschepis@gmail.com>
2021-12-21 11:27:50 -0700

    worked on js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
-var url = "https://pokedex2.p.rapidapi.com/pokedex/us/pikachu";
-var key = "67cb78248emsh1e4e33cf18493bcp19763bjsne115c744c684";
 var mainSection = $('#mainSection');
 
-fetch(url, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "pokedex2.p.rapidapi.com",
-		"x-rapidapi-key": key
-	}
-}).then(function (response) {
-    return response.json();
-}).then(function (data) {
-    console.log(data);
-    //mainSection.text(data[0].name); //print the pokemons name to the page
-});
+
+document.getElementById("searchButton").addEventListener("click", handleSearch);
+
+
+function handleSearch() {
+	var pokename = document.getElementById("searchText").value;
+	
+	var url = "https://pokedex2.p.rapidapi.com/pokedex/us/" + pokename;
+	var key = "67cb78248emsh1e4e33cf18493bcp19763bjsne115c744c684";
+
+	fetch(url, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "pokedex2.p.rapidapi.com",
+			"x-rapidapi-key": key
+		}
+	}).then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		console.log(data);
+		
+		document.getElementById("mainSection").innerHTML = "<h1>" + data[0].name + "</h1>";
+	});
+}
+
