f37bd7eb580571e0f71db544453087ba7bfaab3c
Alix Borja <borja.alixander@gmail.com>
2021-12-21 14:01:39 -0700

    starting battle page

---
+++ assets/js/battle.js
@@ -0,0 +1,30 @@
+var battleSection = $('#battleSection');
+var key = "98c7599992msh5ea6f0b3a380ff5p13b5bbjsn24e983bd9684";
+
+
+
+showBattleScreen()
+
+
+function showBattleScreen() {
+    var randoPokemon
+    var url = "https://pokedex2.p.rapidapi.com/pokedex/us/";
+    
+    fetch(url, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "pokedex2.p.rapidapi.com",
+            "x-rapidapi-key": key
+        }
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        var number = Math.floor(Math.random() * data.length)
+        var outputstring = "<img src='"+data[number].ThumbnailImage+"'>";
+        console.log(number)
+        console.log(outputstring)
+        document.getElementById("battleSection").innerHTML = outputstring
+    });
+    
+    
+    }
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,7 @@ var key = "98c7599992msh5ea6f0b3a380ff5p13b5bbjsn24e983bd9684";
 
 
 document.getElementById("searchButton").addEventListener("click", handleSearch);
-document.addEventListener("keypress", function(e) {
-	if (e.key === 'Enter') {
-		handleSearch();
-	}
-});
-
-
-
+document.querySelector
 showBattleScreen();
 
 
@@ -36,5 +29,21 @@ function handleSearch() {
 }
 
 function showBattleScreen() {
+var randoPokemon
+var url = "https://pokedex2.p.rapidapi.com/pokedex/us/";
+
+fetch(url, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "pokedex2.p.rapidapi.com",
+		"x-rapidapi-key": key
+	}
+}).then(function (response) {
+	return response.json();
+}).then(function (data) {
+	var number = Math.floor(Math.random() * data.length)
+	console.log(number)
+});
+
 
 }
